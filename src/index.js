4bf664ac807491a302d1a71cdfacbe5b67009d94
KBL <kibinlee19k@gmail.com>
2020-01-25 16:38:44 +0900

    test with Tariku

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./Components/App";
 import Client from "./Apollo/Client";
 import { ApolloProvider } from "react-apollo-hooks";
-
+//Test
 ReactDOM.render(
   <ApolloProvider client={Client}>
     <App />
